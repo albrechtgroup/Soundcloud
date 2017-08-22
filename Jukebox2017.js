dc31a1e723b1f4de434e2e8a582d7f9d2174cada
andy albrecht <aalbrecht33733@gmail.com>
2017-08-22 18:01:34 -0400

    player working

--- Jukebox2017.js
+++ Jukebox2017.js
@@ -1,111 +1,85 @@
-////*****  ******    *********//////
+var playButton = document.getElementById('play');
+var pauseButton = document.getElementById('pause');
+var previousButton = document.getElementById('prev');
+var nextButton = document.getElementById('next');
 
-// function Jukebox(audio) {
-//   this.audio = audio;
-//   this.songs = ['http://bit.ly/2sXBeXS', 'http://bit.ly/2tvIFmG'];
-//   this.currentSongIndex = 0;
-//
-//   this.play = function() {
-//     var song = this.songs[this.currentSongIndex];
-//     this.audio.src = song;
-//     this.audio.load();
-//     this.audio.play();
-//   }
-//
-//
-//   this.pause = function() {
-//     this.audio.pause();
-//   }
+SC.initialize({
+    client_id: 'fd4e76fc67798bfa742089ed619084a6'
+  });
 
-////////////////////////////////////////////
+function Jukebox(audio) {
+    this.songStorage = [];
 
+    this.addTrack = function(song) {
+        this.songStorage.push(song);
+    }
 
-var audio = document.getElementById('jukeAudio')
+    this.currentSongIndex = 0;
 
+    this.play = function() {
+        SC.get('/tracks/' +  this.songStorage[this.currentSongIndex]).then(function(tracks) {
+         this.song = SC.stream('/tracks/' + this.songStorage[this.currentSongIndex]);
+         this.song.then(function(player) {
+            this.player = player;
+             player.play();
+           })}
 
+    this.pause = function() {
+        alert("hello pause");
+    }
 
-document.getElementById('play').onclick = function() {
-  audio.src="song1.mp3";
-  audio.play();
+    this.previous = function() {
+        alert("hello previous");
+    }
+
+    this.next = function() {
+        alert("hello next");
+    }
 }
+
+//////////////////////   END OF JUKEBOX  /////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+var audio = document.getElementById('jukeAudio'); //====================Instances Add tracks to storage======
+var jukebox = new Jukebox();
+
+jukebox.addTrack(200954643);
+jukebox.addTrack('hello');
+
+
+
+
+// Event Listeners
+
+
+playButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    jukebox.play();
+    // audio.src="song1.mp3";
+    // audio.play();
+});
 
 document.getElementById('pause').onclick = function() {
-  audio.src="song1.mp3";
-  audio.pause();
-}
+    event.preventDefault();
+    jukebox.pause();
+    // audio.src="song1.mp3";
+    // audio.pause();
+};
 
-document.getElementById('stop').onclick = function() {
-  audio.src="song1.mp3";
-  audio.stop();
-}
+document.getElementById('next').onclick = function() {
+    event.preventDefault();
+    jukebox.next();
+    // audio.src="song1.mp3";
+    // audio.stop();
+};
 
-
-
-
-
-// function container() {
-//   alert('something');
-// }
-//
-// function title() {
-//
-// }
-//
-// function artistImg() {
-//
-// }
-//
-// function jukeboxPlayer() {
-//
-// }
-//
-// function jukoboxInfo() {
-//
-// }
-
-
-
-
-
-
-
-
-////////////////////////////////////////////
-/////////////////////////////////////////////
-
-
-// function Jukebox(songs, artists){
-//   this.songs = songs;
-//   this.artists = artists;
-// }
-//
-// var songs = [];
-// var currentSong = 0;
-// // load track objects into songs array elsewhere..
-// function playNext() {
-//
-// }
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-// function soundclick(play) {
-//
-// }
-// function soundclick(pause) {
-//
-// }
-// function soundclick(rewind) {
-//
-// }
-// function soundclick(ff) {
-//
-// }
-// function soundclick(stop) {
-//
-// }
-
-////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////
+previousButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    jukebox.previous();
+});
 
 /*///////////// Jukebox Starter Kit
 Goal:
@@ -145,3 +119,56 @@ Allow a user to load one of their own songs
 Complete = Meets ALL grading criteria points above.
 
 Incomplete = Does not meet all grading criteria points above. Needs improvement or missing submission.*/
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+//////////   ***SOUNDCLOUD***
+
+// Goal:
+//
+// Successfully implement the SoundCloud API into a current project
+//
+// Assignment:
+//
+// Remember that Jukebox Starter Kit? Well, now it’s time to make it awesome! Take your current jukebox and tweak it so that it can play songs hosted on SoundCloud. This is also the time to make changes to UI/UX of the application so that it looks and feels like a true jukebox.
+//
+// Before you Begin:
+//
+// Create a SoundCloud main account (Links to an external site.)Links to an external site. if you haven't already
+// Use the API Key provided to you by your instructor
+// Read and understand the streaming music example (Links to an external site.)Links to an external site. provided to you by their provided sdk
+// Unfortunately, the SoundCloud 3.0 SDK playback feature uses flash when on Google Chrome and does not work by default. Try disabling the flash plugin through the chrome://plugins/ menu (type it into the location bar).
+// If you intend on the doing the challenge take a look at the playlist documentation (Links to an external site.)Links to an external site.
+// For further documentation check out their API guide (Links to an external site.)Links to an external site.
+// Your SoundCloud enabled jukebox should allow the user to do the following things:
+//
+// Play a track off of SoundCloud based on its track ID
+// Pause the currently playing track
+// Display the following current track information:
+// Artist name with link to his/her profile page
+// Title with link to track's page
+// Description and genre
+// Artwork
+// Playlist Challenge:
+//
+// If you have time and are looking for an extra challenge try the following. Create a playlist on SoundCloud and enable the user to do the following things:
+//
+// Play a track off of a chosen playlist
+// Pause the currently playing track
+// Skip to the next or previous track
+// Jump to the beginning when skipping forward from the last track
+// Jump to the end when skipping backward the first track
+// Display the following playlist information:
+// Creator with link to his/her profile page
+// Playlist title with link to playlist page
+// Artwork
+
+///// Grading criteria:
+//
+// JS throws no errors
+// Pause and play functions work
+// Track information is displayed alongside their corresponding links
+// JukeBox is object-oriented
+// Page has at least one song on load and can play a different song based on track id
+// Allow a user to load one of their own songs
+// HTML/CSS is properly formatted
